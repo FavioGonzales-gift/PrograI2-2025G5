f2e43a0740c7854af0a99b91458c8d680e5ea80d
Belen Manrrique <fabiola.manrrique@ucb.edu.bo>
2025-12-17 16:53:42 -0600

    Add files via upload

--- lib_Informacion_Curso.h
+++ lib_Informacion_Curso.h
@@ -4,64 +4,87 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "lib_CrearCurso.h"
+#include <vector>
+#include "lib_CrearCurso.h"  
 
 using namespace std;
 
-void Mostrar_Inf_Por_Curso(string Archivo_crear_curso)
+void Mostrar_Inf_Por_Curso(string Archivo_crear_curso, string Archivo_Txt_Salida = "Cupos_Por_Curso.txt")
 {
-    ifstream archivo;
-    archivo.open(Archivo_crear_curso, ios::binary);
-    
-    if (!archivo)
+    ifstream archivo_bin(Archivo_crear_curso, ios::binary);
+    ofstream archivo_txt(Archivo_Txt_Salida);  
+
+    if (!archivo_bin)
     {
-        cout << "\nNo se pudo abrir el archivo.\n\n";
+        cout << "\nNo se pudo abrir el archivo binario de cursos.\n\n";
+        system("pause");
+        return;
+    }
+
+    if (!archivo_txt)
+    {
+        cout << "\nNo se pudo crear el archivo de texto de salida.\n\n";
+        archivo_bin.close();
+        system("pause");
         return;
     }
 
     Curso curso;
     bool hayCursos = false;
 
-    cout << "\n";
-    cout << "\t\t---------------LISTA DE CUPOS POR CURSO---------------" << endl;
-    cout << "CÓDIGO\tNIVEL\t\t\tPARALELO\tPROFESOR\tCUPO MÁX.\tDISPONIBLES" << endl;
-    cout << "-------------------------------------------------------------------------------------------------------------" << endl;
-
     vector<string> niveles = {"", "4to de secundaria", "5to de secundaria", "6to de secundaria"};
     vector<string> paralelos = {"", "A", "B"};
 
-    while (archivo.read((char*)&curso, sizeof(Curso)))
+    system("cls");
+    cout << "\n";
+    cout << "\t\t--------------- LISTA DE CUPOS POR CURSO ---------------" << endl;
+    cout << "CODIGO\tNIVEL\t\t\tPARALELO\tPROFESOR\t\t\tCUPO MAX.\tDISPONIBLES" << endl;
+    cout << "------------------------------------------------------------------------------------------------------------------" << endl;
+
+    archivo_txt << "=============== LISTA DE CUPOS POR CURSO ===============" << endl;
+    archivo_txt << "CODIGO\tNIVEL\t\t\tPARALELO\tPROFESOR\t\t\tCUPO MAX.\tDISPONIBLES" << endl;
+    archivo_txt << "------------------------------------------------------------------------------------------------------------------" << endl;
+
+    while (archivo_bin.read((char*)&curso, sizeof(Curso)))
     {
         hayCursos = true;
 
-        string nivel;
-        string paralelo;
-        if (curso.nivel >= 1 && curso.nivel <= 3)
-        {
-            nivel = niveles[curso.nivel];
-        }
-        else
-        {
-            nivel = "Desconocido";
-        }
-        if (curso.paralelo >= 1 && curso.paralelo <= 2)
-        {
-            paralelo = paralelos[curso.paralelo];
-        }
-        else
-        {
-            paralelo = "Error";
-        }
+        string nivel_str = (curso.nivel >= 1 && curso.nivel <= 3) ? niveles[curso.nivel] : "Desconocido";
+        string paralelo_str = (curso.paralelo >= 1 && curso.paralelo <= 2) ? paralelos[curso.paralelo] : "Error";
 
-        cout << curso.codigoCurso << "\t" << nivel << "\t" << paralelo << "\t\t" << curso.profesor << "\t" << curso.cupo_maximo << "\t\t" << curso.cupos_disponibles << endl;
+        cout << curso.codigoCurso << "\t"
+             << nivel_str << "\t\t"
+             << paralelo_str << "\t\t"
+             << curso.profesor << "\t"
+             << curso.cupo_maximo << "\t\t"
+             << curso.cupos_disponibles << endl;
+
+        archivo_txt << curso.codigoCurso << "\t"
+                    << nivel_str << "\t\t"
+                    << paralelo_str << "\t\t"
+                    << curso.profesor << "\t\t"
+                    << curso.cupo_maximo << "\t\t"
+                    << curso.cupos_disponibles << endl;
     }
 
-    archivo.close();
+    cout << "------------------------------------------------------------------------------------------------------------------" << endl;
+    archivo_txt << "------------------------------------------------------------------------------------------------------------------" << endl;
 
     if (!hayCursos)
     {
-        cout << "No hay cursos registrados.\n";
+        cout << "No hay cursos registrados aún.\n";
+        archivo_txt << "No hay cursos registrados aún." << endl;
     }
+    else
+    {
+        cout << "\nReporte generado exitosamente en: " << Archivo_Txt_Salida << endl;
+    }
+
+    cout << "\n";
+    system("pause");
+
+    archivo_bin.close();
+    archivo_txt.close();
 }
 
 #endif
